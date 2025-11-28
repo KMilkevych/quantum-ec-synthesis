8a5593193812f4f5ceadd17f61409e173ff9b9d3
Kostiantyn Milkevych <mkv2510kostya@gmail.com>
2025-11-28 12:27:15 +0100

    Added caching to q-synth circuit optimization

--- src/optimizer/QSynthOptimizer.py
+++ src/optimizer/QSynthOptimizer.py
@@ -1,5 +1,10 @@
+from functools import cache
+from qiskit import qasm2
+
+
 from optimizer.Optimizer import Optimizer
 from qiskit import QuantumCircuit
+from qiskit.circuit.quantumcircuitdata import QuantumCircuitData
 
 import qsynth
 
@@ -8,12 +13,26 @@ class QSynthOptimizer(Optimizer):
     def __init__(self):
         pass
 
+    @cache
+    def _optimize_inner(self, str_circuit: str) -> QuantumCircuit:
 
-    def optimize(self, circuit: QuantumCircuit) -> QuantumCircuit:
+        # Re-construct circuit
+        circuit = QuantumCircuit.from_qasm_str(str_circuit)
+
+        # Optimize circuit
         qc = qsynth.peephole_synthesis(
             circuit=circuit,
             slicing="cnot",
             metric='cx-depth_cx-count',
-            verbose=1,
+            verbose=-1,
         ).circuit
+
         return qc
+
+    def optimize(self, circuit: QuantumCircuit) -> QuantumCircuit:
+
+        # De-construct circuit
+        str_circuit = qasm2.dumps(circuit)
+
+        # Optimize
+        return self._optimize_inner(str_circuit)
